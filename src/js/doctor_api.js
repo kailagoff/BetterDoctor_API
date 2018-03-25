4308f88b4397041d72b754f41e14643d5420adfa
Kaila Goff <kaila.goff@gmail.com>
2018-03-25 15:43:00 -0700

    add html form, minor styling

--- src/js/doctor_api.js
+++ src/js/doctor_api.js
@@ -5,7 +5,6 @@ const apiKey = process.env.exports.apiKey;
 export function getDoctors(){
 
   $.get(`https://api.betterdoctor.com/2016-03-01/doctors?location=45.52313%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
-    console.log(apiKey);
     $('#results').text(` ${response}.`);
   }).fail(function(error) {
     $('#error').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
