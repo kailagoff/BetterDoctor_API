4723ef80f791b29474cbc2c2df9248c85cdbc2ae
Kaila Goff <kaila.goff@gmail.com>
2018-03-25 18:00:36 -0700

    more troubleshooting, fix targeting of API call

--- src/js/doctor_api.js
+++ src/js/doctor_api.js
@@ -8,10 +8,10 @@ export class Doctor{
   const apiKey = process.env.exports.apiKey;
   /* eslint-enable */
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}location=45.52313%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response)
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5234%2C-122.676%2C500&skip=0&limit=20&user_key=e1f4cc75bbdbc6675ccec5b505b8b1aa`).then(function(response)
   {
     showDoctors(response);
-    console.log(showDoctors(response));
+    console.log(response);
   })
     .fail(function(error) {
       $('#error').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
