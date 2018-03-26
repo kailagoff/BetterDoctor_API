5fea700319e87a6504f0d01b0c8d74c3e9be1f68
Kaila Goff <kaila.goff@gmail.com>
2018-03-25 17:48:27 -0700

    try to get apiKey variable working, still not working

--- src/js/doctor_api.js
+++ src/js/doctor_api.js
@@ -8,7 +8,7 @@ export class Doctor{
   const apiKey = process.env.exports.apiKey;
   /* eslint-enable */
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.52313%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=e1f4cc75bbdbc6675ccec5b505b8b1aa`).then(function(response)
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}location=45.52313%2C-122.6765%2C100&user_location=45.5231%2C-122.6765&skip=0&limit=10&user_key=${apiKey}`).then(function(response)
   {
     showDoctors(response);
     console.log(showDoctors(response));
