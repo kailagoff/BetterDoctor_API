65d683de943702ebb09466e13bb8ccc025f64230
Kaila Goff <kaila.goff@gmail.com>
2018-03-25 21:27:29 -0700

    figure out how to encrypt apiKey

--- src/js/doctor_api.js
+++ src/js/doctor_api.js
@@ -5,10 +5,10 @@ export class Doctor{
 
   getDoctors(name, showDoctors){
   /* eslint-disable */
-  const apiKey = process.env.exports.apiKey;
+  const apiKey = `${process.env.exports.apiKey}`;
   /* eslint-enable */
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5234%2C-122.676%2C500&skip=0&limit=20&user_key=e1f4cc75bbdbc6675ccec5b505b8b1aa`).then(function(response)
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5234%2C-122.676%2C500&skip=0&limit=20&user_key=${apiKey}`).then(function(response)
   {
     showDoctors(response);
     console.log(response);
