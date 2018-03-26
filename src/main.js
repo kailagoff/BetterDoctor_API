6272b340f313b1561d539e9ee032559d63816829
Kaila Goff <kaila.goff@gmail.com>
2018-03-25 17:09:24 -0700

    api call working in postman, showing as undefined in program, still troubleshooting

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import './styles.css';
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './js/doctor_api.js';
 
 const showDoctors = function(response) {
+  if (response.data.length == 0) {
+    $("#results").text("No doctors that match your search.")
+  } else {
   for(let i = 0; i < response.data.length; i++) {
     let firstName = response.data[i].profile.first_name;
     let lastName = response.data[i].profile.last_name;
-    $('#results').append(" " + '<li>' + firstName + " " + lastName + '</li>');
+    $('#results').append(`<li> ${firstName} ${lastName} </li>`);
+    }
   }
 }
 
@@ -17,7 +21,7 @@ $(document).ready(function() {
     event.preventDefault();
 
     const medicalIssue = $("#medical-issue").val();
-    const name = $("name").val();
+    const name = $("#name").val();
     console.log(name);
     console.log(medicalIssue);
 
