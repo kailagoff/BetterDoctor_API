eeda68e0d11416289a71871a02652ec2a734d194
Kaila Goff <kaila.goff@gmail.com>
2018-03-28 20:35:26 -0700

    tweak api calls

--- src/js/doctor_api.js
+++ src/js/doctor_api.js
@@ -9,7 +9,7 @@ export class Doctor{
   const apiKey = `${process.env.exports.apiKey}`;
   /* eslint-enable */
 
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5234%2C-122.676%2C500&skip=0&limit=20&user_key=${apiKey}`).then(function(response)
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.5230%2C-122.413%2C25&user_location=45.5230%2C-122.413&skip=0&limit=20&user_key=${apiKey}`).then(function(response)
   {
     showDoctors(response);
     console.log(response);
@@ -24,9 +24,8 @@ export class Doctor{
   /* eslint-disable */
   const apiKey = `${process.env.exports.apiKey}`;
   /* eslint-enable */
-  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?conditions=${medicalIssue}&location=45.523%2C-122.676%2C500&user_location=45.523%2C-122.676&skip=0&limit=10&user_key=${apiKey}`).then(function(response) {
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${medicalIssue}&location=45.5230%2C-122.413%2C25&user_location=45.5230%2C-122.413&skip=0&limit=20&user_key=${apiKey}`).then(function(response) {
     showSymptoms(response);
-    console.log(response);
   })
    .fail(function(error) {
      $('#error').text(`There was an error processing your request: ${error.responseTest}. Please try again.`)
--- src/main.js
+++ src/main.js
@@ -5,23 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './js/doctor_api.js';
 
 
-// const showSymptoms = function(response) {
-//   if (response.data.length == 0) {
-//     $('#errors').text("No doctors match your search.")
-//   } else {
-//       for(let i = 0; i < response.data.length; i++) {
-//         let firstName = response.data[i].profile.first_name;
-//         let lastName = response.data[i].profile.last_name;
-//         let street = response.data[i].practices[i].visit_address.street;
-//         let city = response.data[i].practices[i].visit_address.city;
-//         let state = response.data[i].practices[i].visit_address.state;
-//         let zipcode = response.data[i].practices[i].visit_address.zip;
-//         let newPatient = response.data[i].practices[i].accepts_new_patients;
-//
-//         $('#results').append(`<h6> ${firstName} ${lastName} ${street} ${city} ${state} ${zipcode} ${newPatient}</h6>`);//this line also doesnt work
-//       }
-//     }
-//   }
+const showSymptoms = function(response) {
+  if (response.data.length == 0) {
+    $('#errors').text("No doctors match your search.")
+  } else {
+      for(let i = 0; i < response.data.length; i++) {
+        let firstName = response.data[i].profile.first_name;
+        let lastName = response.data[i].profile.last_name;
+        let street = response.data[i].practices[i].visit_address.street;
+        let city = response.data[i].practices[i].visit_address.city;
+        let state = response.data[i].practices[i].visit_address.state;
+        let zipcode = response.data[i].practices[i].visit_address.zip;
+        let newPatient = response.data[i].practices[i].accepts_new_patients;
+
+        $('#results').append(`<h6> ${firstName} ${lastName} ${street} ${city} ${state} ${zipcode} ${newPatient}</h6>`);//this line also doesnt work
+      }
+    }
+  }
 
 const showDoctors = function(response) {
   if (response.data.length == 0) {
@@ -36,7 +36,7 @@ const showDoctors = function(response) {
       let zipcode = response.data[i].practices[i].visit_address.zip;
       let newPatient = response.data[i].practices[i].accepts_new_patients;
       $('#doctor-results').append(`<h5>${firstName} ${lastName}</h5> <h6>${street} ${city} ${state} ${zipcode}</h6>
-        Takes new Patients?:${newPatient}`);
+      Takes new Patients?:${newPatient}`);
     }
   }
 };
@@ -49,5 +49,14 @@ $(document).ready(function() {
     let newSearch = new Doctor(name);
 
     newSearch.getDoctors(name, showDoctors);
+  });
+
+  $("#speciality-search").submit(function(event) {
+    event.preventDefault();
+
+    const medicalIssue = $("#medical-issue").val();
+    let symptomSearch = new Doctor(medicalIssue);
+
+    symptomSearch.getSpecialities(medicalIssue, showSymptoms);
   });
 });
